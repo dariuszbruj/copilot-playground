110a380c36bbb6b6af6500d4b25d7a2a8a9bdc93
Dariusz Bruj <dbruj@sii.pl>
2024-02-20 22:20:42 +0100

    Added .gitignore file

--- apartments.api/obj/Debug/net8.0/apartments.api.AssemblyInfo.cs
+++ apartments.api/obj/Debug/net8.0/apartments.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apartments.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f336949acfed57ef4b618931948b7dce61177744")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("apartments.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apartments.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
