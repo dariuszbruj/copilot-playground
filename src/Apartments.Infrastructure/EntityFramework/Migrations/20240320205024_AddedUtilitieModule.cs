29a92857213691f6a9407d01b6cae34ae8414cae
Dariusz Bruj <dbruj@sii.pl>
2024-03-20 21:55:16 +0100

    Added missing files

---
+++ src/Apartments.Infrastructure/EntityFramework/Migrations/20240320205024_AddedUtilitieModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Apartments.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUtilitieModule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UtilityMeasurements",
+                columns: table => new
+                {
+                    ApartmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MeasurementDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UtilityType = table.Column<int>(type: "int", nullable: false),
+                    UtilityUsage = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UtilityMeasurements", x => new { x.ApartmentId, x.MeasurementDate, x.UtilityType });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UtilityMeasurements");
+        }
+    }
+}
